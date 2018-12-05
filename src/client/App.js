3988467d0f0133fc6857893cb37fbe8723ea38de
Brandon Paris <brandon@Brandons-MBP.voxel.private>
2018-12-05 18:59:36 -0500

    add node-sass to website

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import { hot } from 'react-hot-loader';
+import '../client/styles/main.scss';
 class App extends Component {
 
   componentDidMount() {
