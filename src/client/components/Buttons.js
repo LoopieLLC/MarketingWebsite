b09130b5ab9bdec369f633bb22743df16fe744c3
simon <fujizhang2525@gmail.com>
2019-01-09 16:46:12 -0800

    button component, also its kinda broken

--- src/client/components/Buttons.js
+++ src/client/components/Buttons.js
@@ -2,5 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonBase from '@material-ui/core/Button';
 
-export default class Base extends React.Component {
+export default class Buttons extends React.Component {
+  
 }
