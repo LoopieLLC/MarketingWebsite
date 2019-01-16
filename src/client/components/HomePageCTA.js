165fd6bfbf5edd72e7172ef8f179e63a4722ff05
simon <fujizhang2525@gmail.com>
2019-01-15 17:02:39 -0800

    navbar and homepage gridwork

--- src/client/components/HomePageCTA.js
+++ src/client/components/HomePageCTA.js
@@ -7,18 +7,14 @@ import CTAImage from '../../../public/images/placeholder1.jpg';
 import Grid from '@material-ui/core/Grid';
 
 const HomePageCTA = ({CTAText, subtext}) => (
+  <main>
   <div className='home-page-cta'>
-    <Grid container spacing={0}>
-      <Grid item xs={12} md={6}>
         <h1 className='cta-text'>{CTAText}</h1>
         <h4 className='cta-subtext'>{subtext}</h4>
         <Button text='Lets get started' />
-      </Grid>
-      <Grid item xs={12} md={6}>
         <img className='cta-image' src={CTAImage} />
-      </Grid>
-    </Grid>
   </div>
+  </main>
 );
 
 HomePageCTA.propTypes = {
