518891d85d9b86fab01ce9c17e10068437d6315a
simon <fujizhang2525@gmail.com>
2019-01-30 13:42:41 -0800

    card style

--- src/client/components/StickyNotes.js
+++ src/client/components/StickyNotes.js
@@ -1,14 +1,22 @@
 //its like the material design cards but more pretentious
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import CardActionArea from '@material-ui/core/CardActionArea';
+
 import {
   HashRouter
 } from 'react-router-dom';
+
+const styles = {
+    borderRadius: 10,
+};
+
 
 export default class StickyNotes extends React.Component {
 
@@ -18,7 +26,7 @@ export default class StickyNotes extends React.Component {
     return <>
       <HashRouter>
        <CardActionArea onClick={event => {}}>
-        <Card className="cards">
+        <Card style={styles} className="cards">
           <CardContent>
             <h3>{this.props.header}</h3>
             <p>{this.props.content}</p>
