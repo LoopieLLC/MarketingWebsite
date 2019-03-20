f7dc1a513b4bdf5756aa1da24275538f465f9dc7
loopiellcdev <loopiellcdev@gmail.com>
2019-03-20 14:57:19 -0700

    added navbar to services

--- src/client/containers/ServicesContainer.js
+++ src/client/containers/ServicesContainer.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import Navbar from '../components/Navbar';
 import {withRouter} from 'react-router';
 
 class ServicesContainer extends React.Component {
+  state = {
+    image: '',
+    navItems: [
+      {
+        name: 'Home',
+        url: '/home',
+      },
+      {
+        name: 'Pricing',
+        url: '/services',
+      },
+      {
+        name: 'About Us',
+        url: '/aboutus',
+      },
+
+    ],
+  }
+
   render() {
     return (
-      <h2>Services</h2>
+      <Navbar items={this.state.navItems}/>
     );
   }
 };
