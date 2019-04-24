fc74ab7eeb90fd73721f9eb3d54c5da4b85917ad
simon <fujizhang2525@gmail.com>
2019-04-24 16:49:13 -0700

    buttons animatino

--- src/client/components/HomePageCTA.js
+++ src/client/components/HomePageCTA.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
 import LoopieButton from './LoopieButton';
 import CTAImage from '../../../public/images/cover.jpg';
 import CTASlogan from '../../../public/images/CTASlogan.svg';
 import StickyNotes from './StickyNotes';
 import {loopieblue, notblack} from './styles';
-import Paper from '@material-ui/core/Paper';
-import {withStyles} from '@material-ui/core/styles';
-import {Spring} from 'react-spring/renderprops';
+import { withStyles } from '@material-ui/core/styles';
+import { Spring } from 'react-spring/renderprops';
 
 const Chevron = () => {
   return (
-    <svg style={{width: 24, height: 24}} viewBox="0 0 24 24">
+    <svg style={{ width: 24, height: 24 }} viewBox="0 0 24 24">
       <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
     </svg>
   );
@@ -28,18 +28,18 @@ const styles = theme => ({
   },
 });
 
-const HomePageCTA = ({subtext, handleEmail, classes}) => (
+const HomePageCTA = ({ subtext, handleEmail, classes }) => (
   <div className="home-page-cta">
     <div className="container split ">
       <Spring
-        from={{opacity: 0, marginLeft: -500}}
-        to={{opacity: 1, marginLeft: 0}}
+        from={{ opacity: 0, marginLeft: -500 }}
+        to={{ opacity: 1, marginLeft: 0 }}
       >
         {props => <div className="split-image cta-image" style={props} />}
       </Spring>
       <Spring
-        from={{opacity: 0, marginLeft: 500}}
-        to={{opacity: 1, marginLeft: 0}}
+        from={{ opacity: 0, marginLeft: 500 }}
+        to={{ opacity: 1, marginLeft: 0 }}
       >
         {props => <div className="cta-slogan" style={props} />}
       </Spring>
@@ -48,11 +48,13 @@ const HomePageCTA = ({subtext, handleEmail, classes}) => (
       <div className="cta-btn">
         <LoopieButton
           color="primary"
-          text="Let&apos;s get started"
+          text="Let's get started"
           className="text-light"
           variant="outlined"
         />
         <button className="button fill"> owo </button>
+        <button className="button outline"> owo </button>
+        <button className="button text"> owo </button>
       </div>
       <div className="learnmore">
         <LoopieButton text="Learn More" icon={<Chevron />}>
@@ -74,4 +76,4 @@ HomePageCTA.propTypes = {
   handleEmail: PropTypes.func,
 };
 
-export default withStyles (styles) (HomePageCTA);
+export default withStyles(styles)(HomePageCTA);
