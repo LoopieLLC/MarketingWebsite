3d0cc3a82a6dff4dce50c4c26f1cdad0b2a4934f
IanCode <iwwhite@pugetsound.edu>
2019-04-27 21:22:10 -0700

    added SplitText component, added component-styles/_split-text.scss, updated main.scss, updated _sticky.scss again with better styling, added split-container-text to _container.scss, added learn more text to StickyNotes.js

---
+++ src/client/components/SplitText.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SplitText = ({header, subtext, type}) => {
+  if(!header){
+    return (
+      <div className={type}>
+        <p>{subtext}</p>
+      </div>
+    );    
+  }
+  return (
+    <div className={type}>
+      <h3>{header}</h3>
+      <p>{subtext}</p>
+    </div>
+  );
+};
+
+SplitText.propTypes = {
+  header: PropTypes.string,
+  subtext: PropTypes.string,
+  type: PropTypes.string,
+};
+
+export default SplitText;
