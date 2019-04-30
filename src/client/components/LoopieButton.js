894e08e6698402f4530951cb300835feb8532c88
simon <fujizhang2525@gmail.com>
2019-04-30 16:44:17 -0700

    fix button alignment with icons

--- src/client/components/LoopieButton.js
+++ src/client/components/LoopieButton.js
@@ -1,22 +1,24 @@
 import React from 'react';
 
-function handleVariant(props){
-    let variant = props.variant;
-    switch(variant){
-        case "filled":
-            return "button fill";
-        case "outlined":
-            return "button outline";
-        case "text":
-            return "button text";
-    }
-};
+function handleVariant(props) {
+  const {variant} = props;
+  switch (variant) {
+    case 'filled':
+      return 'button fill';
+    case 'outlined':
+      return 'button outline';
+    case 'text':
+      return 'button text black';
+  }
+}
 
-function LoopieButton({text, onClick, variant, color, icon}) {
-
-    return(
-        <button className={handleVariant({variant})}>{icon}{text}</button>
-    );
-};
+function LoopieButton({  text, onClick, variant, color, icon  }) {
+  return (
+    <button className={handleVariant({ variant })}>
+      {icon}
+      {text}
+    </button>
+  );
+}
 
 export default LoopieButton;
