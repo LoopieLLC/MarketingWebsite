0027a008d9dc76595a171941661862bfe778747c
IanCode <iwwhite@pugetsound.edu>
2019-04-30 16:15:17 -0700

    removed most material-ui references. It is still in LoopieButton.js and HomePageCTA.js until the new Button component is implemeted.

--- src/client/index.js
+++ src/client/index.js
@@ -1,35 +1,12 @@
 import 'typeface-roboto';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import App from './App';
-import {ParallaxProvider} from 'react-scroll-parallax';
-
-// theming
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#0085FF',
-    },
-
-    secondary: {
-      main: '#FFC700',
-    },
-  },
-  typography: {
-    useNextVariants: true,
-  },
-});
 
 ReactDOM.render(
-  <MuiThemeProvider theme={theme}>
-    <BrowserRouter>
-      <ParallaxProvider>
-        <App />
-      </ParallaxProvider>
-    </BrowserRouter>
-  </MuiThemeProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
