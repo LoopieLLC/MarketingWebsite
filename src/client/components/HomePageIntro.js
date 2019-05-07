593380bf40bb6c4049efb85cff1af6724aaeab93
casey <maxcaseylin@gmail.com>
2019-05-07 15:47:32 -0700

    added potential subtext for stickynotes

--- src/client/components/HomePageIntro.js
+++ src/client/components/HomePageIntro.js
@@ -12,20 +12,20 @@ const HomePageIntro = () => (
       </div>
       <StickyNotes
         to=""
-        header="24 hour turnaround on every order"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        header="24 Hour Turnaround"
+        content="Our service is lightning-fast. Place your order and we'll deliver it back washed 24 hours after pickup."
         type="sticky-basic left"
       />
       <StickyNotes
         to=""
-        header="Sticky Note"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        header="Flat Rates"
+        content="Stop with the hassle of weighing your bag. Our prices are per-bag instead of per-weight."
         type="sticky-basic center"
       />
       <StickyNotes
         to=""
-        header="Sticky Note"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
+        header="No accounts, No BS"
+        content="No more frustration trying to create another account. Just plug in your information and order."
         type="sticky-basic right"
       />
     </div>
