069dc3d92ed9b6e7a886580c3246be01a48f6d2e
casey <maxcaseylin@gmail.com>
2019-05-07 16:05:53 -0700

    djlafs

---
+++ src/client/components/HomePagePricing.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import StickyNotes from './StickyNotes';
+import SplitText from './SplitText';
+
+const HomePagePricing = () => (
+  // TODO: make it look better, add real text, shadows? animations?
+  <React.Fragment>
+    <div className="container full">
+      <div className="title one">
+        <SplitText header="Simple Pricing." type="split-text left" />
+        <SplitText
+          subtext="Our customer plan is simple and easy."
+          type="split-text right"
+        />
+      </div>
+      <StickyNotes
+        to=""
+        header="Wash and Dry(No Fold)"
+        content="We wash your clothes, dry them, and send them back"
+        type="sticky-basic left"
+      />
+      <StickyNotes
+        to=""
+        header="Standard Wash and Fold"
+        content="We wash your clothes, dry them, and fold them before sending them back"
+        type="sticky-basic right"
+      />
+    </div>
+  </React.Fragment>
+);
+
+export default HomePagePricing;
