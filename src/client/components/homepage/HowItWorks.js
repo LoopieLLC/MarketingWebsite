5a544be31f49697c02774debcea1d69916874f42
IanCode <iwwhite@pugetsound.edu>
2019-05-11 13:42:58 -0700

    updated react-dom to version 16.8.6, updated react-hot-loader to version 4.8.4. This fixed some issues with hooks

--- src/client/components/homepage/HowItWorks.js
+++ src/client/components/homepage/HowItWorks.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import StickyNotes from '../StickyNotes';
 import SplitText from '../SplitText';
+import HowSlides from './HowSlides.js';
 
 const HowItWorks = () => (
-  // TODO: make it look better, add real text, shadows? animations?
-  <React.Fragment>
-    <div className="container half-full">
-      <div className="title one">
-        <SplitText header="How it works" type="split-text left" />
-        <SplitText subtext="Our certified washers and drivers make laundry a breeze." type="split-text right" />
-      </div>
-    </div>
-  </React.Fragment>
+	// TODO: make it look better, add real text, shadows? animations?
+	<React.Fragment>
+		<div className="container full">
+			<div className="title one">
+				<SplitText header="How it works" type="split-text left" />
+				<SplitText
+					subtext="Our certified washers and drivers make laundry a breeze."
+					type="split-text right"
+				/>
+			</div>
+			<HowSlides/>
+		</div>
+	</React.Fragment>
 );
 
 export default HowItWorks;
--- src/client/components/homepage/HowSlides.js
+++ src/client/components/homepage/HowSlides.js
@@ -11,7 +11,7 @@ function HowSlides() {
 		enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
 		leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
 	});
-	return <div className="container full" />;
+	return <div />;
 }
 
 export default HowSlides;
