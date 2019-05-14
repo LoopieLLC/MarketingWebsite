34836f54f2fd2afa9e2d1d9c2d1dbada6c9be058
simon <fujizhang2525@gmail.com>
2019-05-14 15:07:25 -0700

    partner

--- src/client/components/homepage/PartnerLanding.js
+++ src/client/components/homepage/PartnerLanding.js
@@ -5,7 +5,7 @@ import bungalow from '../../../../public/images/bungalow.png';
 const PartnerLanding = () => (
   // TODO: make it look better, add real text, shadows? animations?
   <React.Fragment>
-    <div className="container full">
+    <div className="container full full-height">
       <div className="partner-header">
         <span className="partner-text black">Better, </span>
         <span className="partner-text blue">Together.</span>
