f9cac3b91cecc760ff48ace003b151236aa5a700
simon <fujizhang2525@gmail.com>
2019-05-14 16:24:50 -0700

    ad fonts

--- src/client/components/homepage/PartnerLanding.js
+++ src/client/components/homepage/PartnerLanding.js
@@ -6,12 +6,12 @@ const PartnerLanding = () => (
   // TODO: make it look better, add real text, shadows? animations?
   <React.Fragment>
     <div className="container full full-height">
-      <div className="partner-header">
-        <h3 className="partner-text black">
+      <div className="partner header">
+        <h1 className="partner-text black">
           Better, <span className="blue">Together.</span>
-        </h3>
+        </h1>
+        <SplitLogo partner={bungalow} partneralt="Bungalow" />
       </div>
-      <SplitLogo partner={bungalow} partneralt="Bungalow" />
     </div>
   </React.Fragment>
 );
