af495046ac8e2173a2ef1f601843ff5a9d4eef7e
IanCode <iwwhite@pugetsound.edu>
2019-05-21 16:43:45 -0700

    removed reference to removed page

--- src/client/components/homepage/SlideShow.js
+++ src/client/components/homepage/SlideShow.js
@@ -1,26 +1,70 @@
 import StickyNotes from '../StickyNotes';
-import SplitText from '../SplitText'; 
+import SplitText from '../SplitText';
 import React, { useState, useCallback, useEffect } from 'react';
 import { useTransition, animated } from 'react-spring';
-import HowSlides from './HowSlides';
 import { loopieblue, loopieyellow } from '../styles';
 
 const pages = [
-  ({ style }) => <animated.div className="slide-background-test order" style={{ ...style }}></animated.div>,
-  ({ style }) => <animated.div className="slide-background-test pickup" style={{ ...style }}></animated.div>,
-  ({ style }) => <animated.div className="slide-background-test washer" style={{ ...style }}></animated.div>,
-  ({ style }) => <animated.div className="slide-background-test fold" style={{ ...style }}></animated.div>,
-  ({ style }) => <animated.div className="slide-background-test deliver" style={{ ...style }}></animated.div>,
-]
+	({ style }) => (
+		<animated.div
+			className="slide-background-test order"
+			style={{ ...style }}
+		/>
+	),
+	({ style }) => (
+		<animated.div
+			className="slide-background-test pickup"
+			style={{ ...style }}
+		/>
+	),
+	({ style }) => (
+		<animated.div
+			className="slide-background-test washer"
+			style={{ ...style }}
+		/>
+	),
+	({ style }) => (
+		<animated.div className="slide-background-test fold" style={{ ...style }} />
+	),
+	({ style }) => (
+		<animated.div
+			className="slide-background-test deliver"
+			style={{ ...style }}
+		/>
+	),
+];
 
 const pages2 = [
-  
-  ({ style }) => <animated.div className="slide-left test" style={{ ...style }}><h3 className="slide-text">Place an order with your phone</h3></animated.div>,
-  ({ style }) => <animated.div className="slide-left test" style={{ ...style }}><h3 className="slide-text">A driver will come to pick up your clothes</h3></animated.div>,
-  ({ style }) => <animated.div className="slide-left test" style={{ ...style }}><h3 className="slide-text">The driver takes your clothes to a certified washer</h3></animated.div>,
-  ({ style }) => <animated.div className="slide-left test" style={{ ...style }}><h3 className="slide-text">Your clothes are washed and folded</h3></animated.div>,
-  ({ style }) => <animated.div className="slide-left test" style={{ ...style }}><h3 className="slide-text">A driver delivers your washed, folded clothes to you in under 24 hours</h3></animated.div>,
-]
+	({ style }) => (
+		<animated.div className="slide-left test" style={{ ...style }}>
+			<h3 className="slide-text">Place an order with your phone</h3>
+		</animated.div>
+	),
+	({ style }) => (
+		<animated.div className="slide-left test" style={{ ...style }}>
+			<h3 className="slide-text">A driver will come to pick up your clothes</h3>
+		</animated.div>
+	),
+	({ style }) => (
+		<animated.div className="slide-left test" style={{ ...style }}>
+			<h3 className="slide-text">
+				The driver takes your clothes to a certified washer
+			</h3>
+		</animated.div>
+	),
+	({ style }) => (
+		<animated.div className="slide-left test" style={{ ...style }}>
+			<h3 className="slide-text">Your clothes are washed and folded</h3>
+		</animated.div>
+	),
+	({ style }) => (
+		<animated.div className="slide-left test" style={{ ...style }}>
+			<h3 className="slide-text">
+				A driver delivers your washed, folded clothes to you in under 24 hours
+			</h3>
+		</animated.div>
+	),
+];
 
 function SlideShow() {
 	const [index, setIndex] = useState(0);
@@ -43,22 +87,22 @@ function SlideShow() {
 		}, 4000);
 		return () => clearTimeout(timeout);
 	}, [index]);
-  return (
-  	<React.Fragment>
-	    <div className="slide" onClick={onClick}>
-	      {transitions.map(({ item, props, key }) => {
-	        const Page = pages[item]
-	        return (<Page key={key} style={props} />)
-	      })}
-	    </div>
-	    <div className="slide-left" onClick={onClick}>
-	      {transitions2.map(({ item, props, key }) => {
-	        const PageLeft = pages2[item]
-	        return (<PageLeft key={key} style={props} />)
-	      })}
-	    </div>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<div className="slide" onClick={onClick}>
+				{transitions.map(({ item, props, key }) => {
+					const Page = pages[item];
+					return <Page key={key} style={props} />;
+				})}
+			</div>
+			<div className="slide-left" onClick={onClick}>
+				{transitions2.map(({ item, props, key }) => {
+					const PageLeft = pages2[item];
+					return <PageLeft key={key} style={props} />;
+				})}
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default SlideShow;
