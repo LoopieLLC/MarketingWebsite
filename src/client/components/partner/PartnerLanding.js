fe2ecf47fb197fd0c377a4416482861d6fc1a6d3
IanCode <iwwhite@pugetsound.edu>
2019-05-22 14:54:42 -0700

    added linting to work with sublime

--- src/client/components/partner/PartnerLanding.js
+++ src/client/components/partner/PartnerLanding.js
@@ -12,7 +12,7 @@ const PartnerLanding = () => (
         </h1>
         <SplitLogo partner={bungalow} partneralt="Bungalow" />
       </div>
-      <div className = "partner-description">Loopie and Bungalow; the perfect match. Loopie's wash and fold laundry delivery service is perfect for motivated, mobile Bungalow residents. Use code GO_BUNGALOW now to receive your first order free, and free bags for your duration of Loopie Laundry. All Loopie orders are delivered within 24 hours, so you can save time and focus on what really matters. </div>
+      <div className = "partner-description">{"Loopie and Bungalow; the perfect match. Loopie\'s wash and fold laundry delivery service is perfect for motivated, mobile Bungalow residents. Use code GO_BUNGALOW now to receive your first order free, and free bags for your duration of Loopie Laundry. All Loopie orders are delivered within 24 hours, so you can save time and focus on what really matters." }</div>
     </div>
   </React.Fragment>
 );
