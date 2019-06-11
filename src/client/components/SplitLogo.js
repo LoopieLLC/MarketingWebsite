cab5d38e5ff48cfc00401a6d53120f10c76b688f
IanCode <iwwhite@pugetsound.edu>
2019-06-11 14:41:28 -0700

    updated bungalow page with loopie duffel image and new text outlining the promotion

--- src/client/components/SplitLogo.js
+++ src/client/components/SplitLogo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Proptypes from 'prop-types';
 import logo from '../../../public/images/logo.svg';
+import duffel from '../../../public/images/duffel.png'
 
 function SplitLogo({partner, partneralt}) {
   return (
     <span className="split-loopie">
       <img className="logo-left" src={partner} alt={partneralt} />
       <h3 className="logo-text"> + </h3>
+      <h3 className="image-text"><img src={duffel} style={{maxWidth:200}}></img></h3>
       <img className="logo-right" src={logo} alt="Loopie Laundry" />
     </span>
   );
