4ae7449aafef538a41742394ad46d4243cea99bc
IanCode <iwwhite@pugetsound.edu>
2019-06-11 15:20:15 -0700

    duffel image resize

--- src/client/components/SplitLogo.js
+++ src/client/components/SplitLogo.js
@@ -8,7 +8,7 @@ function SplitLogo({partner, partneralt}) {
     <span className="split-loopie">
       <img className="logo-left" src={partner} alt={partneralt} />
       <h3 className="logo-text"> + </h3>
-      <h3 className="image-text"><img src={duffel} style={{maxWidth:200}}></img></h3>
+      <h3 className="image-text"><img src={duffel} style={{maxWidth:300}}></img></h3>
       <img className="logo-right" src={logo} alt="Loopie Laundry" />
     </span>
   );
