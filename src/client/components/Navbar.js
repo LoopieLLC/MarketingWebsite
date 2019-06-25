0a5f4b55410d187498eb73fe6a97b533ef1a429a
simon <fujizhang2525@gmail.com>
2019-06-25 15:27:29 -0700

    menu functions

--- src/client/components/Navbar.js
+++ src/client/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavbarItem from './NavbarItem';
 import logo from '../../../public/images/logo.svg';
 import LoopieButton from './LoopieButton';
+import HamburgerMenu from 'react-hamburger-menu';
 
 const NAVIGATION_ITEMS = [
   {
@@ -18,10 +19,25 @@ const NAVIGATION_ITEMS = [
   },
 ];
 
-const Navbar = (props) => {
+function Navbar(props) {
+  let [open, setOpen] = useState(0);
+
   return (
     <nav className="navbar main-nav fixed">
       <img className="logo" src={logo} alt="Loopie Laundry" />
+      <div className="nav-mobile">
+        <HamburgerMenu
+          isOpen={open}
+          menuClicked={() => setOpen((open = !open))}
+          width={24}
+          height={20}
+          strokeWidth={2}
+          rotate={0}
+          color="white"
+          borderRadius={2}
+          animationDuration={0.3}
+        />
+      </div>
       <ul className="nav-item-container">
         {NAVIGATION_ITEMS.map((item) => (
           <NavbarItem to={item.url} key={item.name} item={item} />
@@ -38,6 +54,6 @@ const Navbar = (props) => {
       </ul>
     </nav>
   );
-};
+}
 
 export default Navbar;
