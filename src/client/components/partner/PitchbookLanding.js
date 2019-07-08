eb5991015c6c21c3950a02ca0b85ba59baa093ae
IanCode <iwwhite@pugetsound.edu>
2019-07-08 14:53:53 -0700

    minor style changes

--- src/client/components/partner/PitchbookLanding.js
+++ src/client/components/partner/PitchbookLanding.js
@@ -6,7 +6,7 @@ import PartnerCode from '../PartnerCode';
 const PitchbookLanding = () => (
   // TODO: make it look better, add real text, shadows? animations?
   <React.Fragment>
-    <div className="container full full-height">
+    <div className="container full-pitchbook full-height">
       <div className="partner">
         <h1 className="partner-text black">
           Better, <span className="blue">Together.</span>
@@ -15,11 +15,10 @@ const PitchbookLanding = () => (
         <div className="partner description">
           <p>
             Loopie has been serving customers in the Seattle area for over a year, and since our 
-            start we have grown and changed immensely. There are few who understand 
-            startups like the folks at Pitchbook, and we hope you'll try our wash and fold 
-            laundry delivery service and get as excited about it as we are. In addition to 
-            receiving a free Loopie Duffel, you will get $10.00 off your first order if you 
-            sign up now with code: Pitchbook.{' '}
+            start we have grown and changed immensely. There are few who understand startups like
+            the folks at Pitchbook, and we hope you'll try our wash and fold laundry delivery service 
+            and get as excited about it as we are. In addition to receiving a free Loopie Duffel, 
+            you will get $10.00 off your first order if you sign up now with code: Pitchbook.{' '}
           </p>
           <PartnerCode code="Pitchbook" />
         </div>
