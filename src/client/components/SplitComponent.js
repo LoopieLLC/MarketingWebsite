fe6a3dedda836840f4c5a00bea645775ec3653d6
maxcaseylin <maxcaseylin@gmail.com>
2019-08-02 20:54:29 -0700

    added split-component divs

--- src/client/components/SplitComponent.js
+++ src/client/components/SplitComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import StickyNotes from './StickyNotes';
 import Proptypes from 'prop-types';
 
 // will be a generic split-component
 const SplitComponent = (props) => {
   return (
     <div className="split-component">
-      <h1>bruh</h1>
+      <div className="left">
+        <StickyNotes header="yo" type="sticky" />
+      </div>
+      <div className="right">
+        <h3>yo</h3>
+      </div>
     </div>
   );
 };
